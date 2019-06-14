cc639cb06973c37738ab50948da4820ecfb2777d
root <root@technerd842c.mylabserver.com>
2019-06-14 11:13:55 +0000

    Add log path

--- manifests/params.pp
+++ manifests/params.pp
@@ -8,7 +8,7 @@ class nginx::params {
   $config_group = 'root'
   $config_confd = "${config_dir}/conf.d"
   $config_log_dir = '/var/log/nginx'
-  $config_pid_file = '/run/var/nginx.pid'
+  $config_pid_file = '/run/nginx.pid'
   $config_vdir_enable = $facts['os']['family'] ? {
     'Debian' => "${config_dir}",
     default => undef,
